e0cd5aa30afc6244078fa2908b498f66cc575a53
Lee Apalat <alsapalat@multisyscorp.com>
2020-04-14 19:57:11 +0800

    🚀 update version

--- dist/store/setup.js
+++ dist/store/setup.js
@@ -3,7 +3,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
 import epics from './epics';
 import reducers from './reducers';
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'production' ? compose : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+if (sessionStorage.getItem('_dev_mode') === '1') console.log(`ENVIRONMENT: ${process.env.NODE_ENV}`);
 export default function configureStore() {
   const epicMiddleware = createEpicMiddleware();
   const store = createStore(reducers, composeEnhancers(applyMiddleware(epicMiddleware)));
