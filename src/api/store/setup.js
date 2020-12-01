06aa7b2f0049cc6323e7069caecef98e776173b4
alsapalat <alsapalat@multisyscorp.com>
2020-12-01 14:18:59 +0800

    disable dev_mode console log

--- src/api/store/setup.js
+++ src/api/store/setup.js
@@ -9,7 +9,7 @@ const composeEnhancers = process.env.NODE_ENV === 'production'
   ? compose
   : (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose);
 
-if (sessionStorage.getItem('_dev_mode') === '1') console.log(`ENVIRONMENT: ${process.env.NODE_ENV}`);
+// if (sessionStorage.getItem('_dev_mode') === '1') console.log(`ENVIRONMENT: ${process.env.NODE_ENV}`);
 
 export default function configureStore() {
   const epicMiddleware = createEpicMiddleware();
